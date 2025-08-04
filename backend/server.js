aa985c0ba952500e70185c3dc8f25e0ef37737f6
gaurav dalakoti <gauravdalakoti046@gmail.com>
2025-08-04 13:07:43 +0530

    configure vercel.json file

--- backend/server.js
+++ backend/server.js
@@ -25,4 +25,9 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
